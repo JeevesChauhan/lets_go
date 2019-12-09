e862f0270a2440e989ed2bc8a6cf5b0fa9c603c3
Jeeves Chauhan <jeeves.chauhan@outlook.com>
2019-12-09 17:21:38 +0000

    Login sessions now work responsively to the navigation bar, allowing users to sign in if they haven't and sign out if they are@

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  #Only when configure_permitted_parameters when using devise controllers
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:trainer])
+end
+
+
 end
